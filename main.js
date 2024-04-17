b6a771637a5d2648eaa7ad6841aa22fa4daf656a
Derek0422 <165080043+Derek0422@users.noreply.github.com>
2024-04-17 21:17:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function btn() {
+    document.getElementById("money")
+    .style.display="none";
+    document.getElementById("sucess")
+    .style.visibility="visible";
+    // document.getElementById("demo")
+    // .style.display="none";
+}
